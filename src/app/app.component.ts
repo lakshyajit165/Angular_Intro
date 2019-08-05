aae64357f73f14e92bb489f8a7525eeaa9ebda11
Lakshyajit <lakshyajit165@gmail.com>
2019-08-05 13:00:41 +0530

    Pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
-  public name = "LK";
-  public message = "";
+  title = 'Angular Learning';
+
 }
