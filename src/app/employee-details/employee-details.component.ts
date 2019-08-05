2c2b5a2275895ffc9a17324f33c8b013f88b3b3b
Lakshyajit <lakshyajit165@gmail.com>
2019-08-05 15:48:05 +0530

    Http error handling

--- src/app/employee-details/employee-details.component.ts
+++ src/app/employee-details/employee-details.component.ts
@@ -9,12 +9,15 @@ import { EmployeeService } from '../employee.service';
 export class EmployeeDetailsComponent implements OnInit {
 
   public employees = [];
-
+  public errorMsg;
+  
   constructor(private _employeeService: EmployeeService) { }
 
+  
   ngOnInit() {
     this._employeeService.getEmployees()
-    .subscribe(data => this.employees = data);
+    .subscribe(data => this.employees = data,
+               error => this.errorMsg = error);
    }
 
 }
