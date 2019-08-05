74aac42675ff323642ca5afc1e62e5d918c559dd
Lakshyajit <lakshyajit165@gmail.com>
2019-08-05 11:32:30 +0530

    reference variables

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -6,29 +6,23 @@ import { Component, OnInit } from '@angular/core';
     <h2>
       Welcome {{ name }}
     </h2>
-    <button (click)="onClick($event)">Greet</button>
-    <button (click)="greeting='Welcome Elkay'">Greet 2</button>
-    <p>{{greeting}}</p>
+    <input #myinput type="text">
+    <button (click)="logMessage(myinput.value)">Log</button>
     `,
   styles: []
 })
 export class TestComponent implements OnInit {
 
   public name = "Elkay";
-  public greeting = "";
+ 
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  onClick(event){
-    console.log(event);
-    this.greeting = "Welcome";
-  }
-
-  greetUser(){
-    return "Hello "+ this.name;
+  logMessage(value){
+    console.log(value);
   }
 
 }
